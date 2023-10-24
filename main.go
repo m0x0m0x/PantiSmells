21a06166e3bf349115045867af4accd0375ed332
m0x0m0x <123001669+m0x0m0x@users.noreply.github.com>
2023-10-24 15:46:34 +0000

    .

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
-/*
-Testing out go get install method 
-*/
 
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
+	helpFlag := flag.Bool("help", false, "Display help")
+	flag.Parse()
+
+	if *helpFlag {
+		displayHelp()
+		os.Exit(0)
+	}
+
 	fmt.Println("SmellPussy")
 	SmellPussy()
+}
+
+func displayHelp() {
+	fmt.Println("This is the help message.")
+	// Add more information about how to use your application
 }
